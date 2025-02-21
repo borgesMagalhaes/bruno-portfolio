9705aa1f6802dbe31582c7f96cc31cab3a3b040b
Bruno Borges Magalhães <borges.magalhaes@gmail.com>
2025-02-21 09:10:41 -0300

    Correção para exportação estática no Next.js 15

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@
 const nextConfig = {
   output: "export",
   distDir: "dist",
-  basePath: "/bruno-portfolio", // Nome do repositório no GitHub
+  // basePath: "/bruno-portfolio", // Nome do repositório no GitHub
   trailingSlash: true,
+  assetPrefix: "https://brunomagalhaes.dev.br/",
   images: {
     unoptimized: true, // Evita erro com otimização de imagens no GitHub Pages
   },
