fbdec83d8157703eb3a66e31418cbee20e1c15a9
Bruno Borges Magalhães <borges.magalhaes@gmail.com>
2025-02-21 00:47:43 -0300

    Configuração para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig = {
   distDir: "dist",
   basePath: "/bruno-portfolio", // Nome do repositório no GitHub
   trailingSlash: true,
+  images: {
+    unoptimized: true, // Evita erro com otimização de imagens no GitHub Pages
+  },
 };
 
 module.exports = nextConfig;
