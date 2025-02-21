a2d08f5d1e6b6d2190cc394c603fe30bb260e5f1
Bruno Borges Magalhães <borges.magalhaes@gmail.com>
2025-02-21 00:42:22 -0300

    Configuração para GitHub Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+"use client";
+
 import React from "react";
 import { FaLinkedin, FaFileAlt, FaInstagram } from "react-icons/fa";
 import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-"use client";
 
 export default function Home() {
   return (
