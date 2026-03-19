e1deb44543302b669a444ed56e1db13eec3e5b3b
Bruno Borges Magalhães <borges.magalhaes@gmail.com>
2026-03-19 08:30:15 -0300

    chore(dist): sync static export artifacts

---
+++ dist/static/chunks/app_layout_tsx_0a548d63._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__2f249c59._.css",
+  "static/chunks/_0b0de77e._.js"
+],
+    source: "dynamic"
+});
